b1a0d253beea51af4215e337098bf0e9d877bcef
5taukeer <38272844+5taukeer@users.noreply.github.com>
2020-04-15 11:14:33 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,17 @@
+a = 3
+b = 6
+# 1. Swap with third variable
+# temp = a
+# a = b
+# b = temp
+
+# 2. Swap without third variable
+# a = a+b
+# b = a-b
+# a = a-b
+
+# 3. Swap with new technique
+a, b = b, a
+
+print(a)
+print(b)
